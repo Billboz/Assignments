9e90de6e2a4666d36508a9796954d94ed22b2dff
billboz1@gmail.com <billboz1@gmail.com>
2014-09-25 16:37:46 -0400

    Assignment_9 RegEx solutions #9

---
+++ assignment_9/solutions_9.rb
@@ -0,0 +1,25 @@
+bmac:assignment_9 bill$ ruby assignment_9.rb
+Hello
+World
+Hello
+World
+Peter Piper
+ed a peck of
+led peppers.
+A peck of
+led peppers Peter Piper
+ed.
+If Peter Piper
+ed a peck of
+led peppers,
+Where's the peck of
+led peppers that Peter Piper
+ed?
+I have brains in Ir head. I have feet in Ir shoes. I can steer Irself any direction I choose. I're on Ir own. And I know what I know. And I are the one who'll decide where to go...
+8
+1
+MatchData
+He
+Regexp
+String
+bmac:assignment_9 bill$
