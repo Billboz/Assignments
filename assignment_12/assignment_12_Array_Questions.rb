1bd4e55226d6dce0067ccd30278aaee2697814d2
billboz1@gmail.com <billboz1@gmail.com>
2014-09-29 23:39:30 -0400

    Assignment 12 update #12

--- assignment_12/assignment_12_Array_Questions.rb
+++ assignment_12/assignment_12_Array_Questions.rb
@@ -1,11 +1,17 @@
 #!/usr/local/bin/ruby/
-#Vars - question count Int (qcount), input count Array (icount), input number Int (inum), input String, questions String,
-#Loop of arrayed questions that capture an array of answers and prints them with the questions.
+
+=begin
+Assignment 12
+Create an array of questions
+Ask a question of the user, from the array
+Continue asking questions until the user enters without giving an answer
+When you run out of questions, start over
+For extra credit: Save all the answers with the questions, and print a summary at the end
+=end
 
 system "clear"
 
-puts "I will ask you questions until you give up!"
-puts
+puts "I will ask you questions until you give up!\n\n"
 puts "Press enter to begin."
 gets
 
@@ -14,23 +20,42 @@ questions = ["What is 1?[0]", "What is 2?[1]", "What is 3?[2]", "What is 4?[3]"]
 #array.each_with_index{|val, index| puts "#{val} => #{index}"}
 
 qcount = 0
-icount = 1
-inum = Array.new
+
+
+user_ans = Array.new
+user_count = 0
 
  loop do
-  puts
   puts questions[qcount] #int
-#  puts "questions is a #{questions.class} index number is #{qcount}"
   input = $stdin.gets.chomp #string
-#  puts input
-#  puts "input is a #{input.class}"
-  inum[icount] = input #inum is an array
-#  puts "inum is an #{inum.class} icount number is #{icount}"
+  user_ans[user_count] = input #inum is an array
+  break if input == ""
+
+  if questions.size - 1 > qcount
+    qcount += 1
+  else
+    qcount = 0
+  end
+  user_count += 1
+end
+
+ user_ans.each do |output|
+   puts " #{questions}#{output}"
+ end
 
 
-  qcount +=1 #somehow I had this increment and the questions cycled through more than once but now they don't???
-  icount +=1
+
+#break if input
+
+#  qcount +=1 #somehow I had this increment and the questions cycled through more than once but now they don't???
+#  icount +=1
+#end
+
 =begin
+icount = 0
+inum = Array.new
+
+
 
   if input != ""
     puts "continue"
@@ -43,6 +68,7 @@ inum = Array.new
 =end
 
 
+
 #  puts "Here is the question #{questions} and the answer #{input}" #This doesn't work
 #  break if input == ""
 #  qcount +=1
@@ -50,16 +76,3 @@ inum = Array.new
 #  break if input == ""
 #  icount -1
 #  puts "inum is an #{inum.class} icount final number is #{icount}"
-
-end
-
-
-
-=begin
-Assignment 12
-Create an array of questions
-Ask a question of the user, from the array
-Continue asking questions until the user enters without giving an answer
-When you run out of questions, start over
-For extra credit: Save all the answers with the questions, and print a summary at the end
-=end
