d71fde7d96858fa0c585761e3abb2efe811628f7
billboz1@gmail.com <billboz1@gmail.com>
2014-10-07 11:14:17 -0400

    Assignment 25 Class Work #25

---
+++ assignment_25/assignment_25_Class_Work.rb
@@ -0,0 +1,36 @@
+class Employee
+
+  @employee_name = nil
+  @employee_city = nil
+  @company_name = nil
+  @company_city = nil
+  @employees = []
+
+  attr_accessor :name
+  attr_accessor :city
+end
+
+class Company
+
+  attr_accessor :name
+  attr_accessor :city
+  attr_accessor :employees
+
+  # Adds the passed employee to the company employee list
+  def hire_employee(employee)
+    self.employees
+    @employees
+  end
+
+  # Removes the passed employee from the company employee list
+  def fire_employee(employee)
+    
+  end
+end
+
+obj = Employee.new
+obj.employee_name = 'Brian Burridge'
+obj.employee_city = 'Clearwater'
+# obj.employees = ?
+obj.hire_employee(obj)
+obj.fire_employee(obj)
