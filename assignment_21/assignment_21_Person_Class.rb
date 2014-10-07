5fe6926c56a112d20a5cfc8f9c0d536a6ac913c6
billboz1@gmail.com <billboz1@gmail.com>
2014-10-07 11:12:52 -0400

    Assignment 21 Person Class #21

---
+++ assignment_21/assignment_21_Person_Class.rb
@@ -0,0 +1,23 @@
+require 'pry'
+class Person
+  #@@person_count = 0
+  attr_reader :first_name
+  attr_reader :last_name
+  attr_accessor :birthday
+
+  def name
+    @name
+  end
+
+
+
+
+private
+
+
+
+
+
+end
+
+binding.pry
