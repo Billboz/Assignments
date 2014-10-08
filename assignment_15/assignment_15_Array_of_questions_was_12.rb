d33d9084c2f5c4d58be599f6813b1b590eea05a0
billboz1@gmail.com <billboz1@gmail.com>
2014-10-08 08:02:22 -0400

    Assignment 15 Array of Questions Latest corrections #16

--- assignment_15/assignment_15_Array_of_questions_was_12.rb
+++ assignment_15/assignment_15_Array_of_questions_was_12.rb
@@ -9,8 +9,8 @@ When you run out of questions, start over
 For extra credit: Save all the answers with the questions, and print a summary at the end
 =end
 
-system "clear"
-
+#system "clear"
+#require pry
 puts "I will ask you questions until you give up!\n\n"
 puts "Press enter to begin."
 gets
@@ -22,7 +22,7 @@ number_of_questions = 0
 
 user_ans = Array.new
 user_count = 0
-
+#binding.pry
 loop do
 
   puts questions[index]
@@ -31,10 +31,26 @@ loop do
 
   user_ans[user_count] = input
 
-  break if input == ""
+  if input == ""
+    puts  user_ans[user_count]
+    break
+  end
+  user_count += 1
   if index >= questions.size - 1
     index = 0
   else
     index +=1
+
   end
 end
+#num = 0
+#binding.pry
+for num in 1..questions.size
+    puts "#{questions[num]}" + " #{user_ans[num]}"
+    num +=2
+  #  puts "#{questions[0]}" + "#{user_ans[0]}" The data exists but I haven't figure out how to access [0] of the array
+  #  puts "#{questions[1]}" + "#{user_ans[1]}"  Also the incrementer doesn't seem to be working since what is printed
+  #  puts "#{questions[2]}" + "#{user_ans[2]}"   is only incremented by 1??
+  #  puts "#{questions[3]}" + "#{user_ans[3]}"
+end
+#  puts "#{questions[num]} #{user_ans[num]}"
