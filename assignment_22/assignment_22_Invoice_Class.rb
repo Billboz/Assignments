2658086b82ea066ffaf478d5f51c205d1cde2a3b
billboz1@gmail.com <billboz1@gmail.com>
2014-12-17 08:56:04 -0500

    Assignement 22 #22

--- assignment_22/assignment_22_Invoice_Class.rb
+++ assignment_22/assignment_22_Invoice_Class.rb
@@ -1,30 +1,42 @@
 #!usr/bin/ruby
 #require 'pry'
+require 'active_support/all'
 
-load 'Invoice_Classes.rb'
-invoice = Invoice.new
+class InvoiceItem
+  attr_accessor :name, :price, :quantity
+end
+
+class Invoice
+  attr_accessor :tax, :items
+
+  def total
+    items.sum { |i| (i.quantity * i.price)}
+  end
+end
+
+ invoice = Invoice.new
+   puts 'What is the tax in your county?'
+   invoice.tax = $stdin.gets.strip.to_f.round(2) / 100
+
+invoice.items = Array.new
 
 loop do
     invoice_item = InvoiceItem.new
-
-  puts 'What is product name?'
-    invoice_item.name          = $stdin.gets.chomp
+  puts 'What is product name? Press enter to quit.'
+    invoice_item.name = $stdin.gets.strip
     break if invoice_item.name == ""
   puts 'What is the sale price?'
-    invoice_item.price         = $stdin.gets.chomp
+    invoice_item.price = $stdin.gets.strip.to_f.round(2)
   puts 'How many would you like?'
-    invoice_item.quantity      = $stdin.gets.chomp
+    invoice_item.quantity = $stdin.gets.strip.to_i
 
   invoice.items << invoice_item
-
-  #puts "invoice_item.total  #{invoice_item.total}"
-  #puts "invoice.item        #{invoice.items}"
-  puts "invoice.tax         #{invoice.tax}"
-
 end
 
-  #puts "invoice.item        #{invoice.items}"
-  invoice.subtotal
+invoice.items.each do |item|
+  puts "Product: #{item.name}\tPrice: $#{item.price}\tQuantity: #{item.quantity}"
+end
 
-
-
+puts "Subtotal: $#{invoice.total.to_f.round(2)}"
+puts "Tax (#{(invoice.tax * 100).round(2)}%): $#{(invoice.total * invoice.tax).round(2)}"
+puts "Total: $#{(((1 + invoice.tax.round(2))* invoice.total)).round(2)}"
